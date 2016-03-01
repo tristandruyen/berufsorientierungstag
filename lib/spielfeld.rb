0d9de63e2a3520d300a697627cf29329f232b3d4
Tristan Druyen <tristan.druyen@invision.de>
2016-03-01 16:22:16 +0100

    more STUFF

--- lib/spielfeld.rb
+++ lib/spielfeld.rb
@@ -1,18 +1,23 @@
 # coding: utf-8
 
-TILESIZE=30
-BORDER=40
+TILESIZE=27
+BORDER=25
 
 module Berufsorientierungstag
+  class DummyElement
+    def initialize
+    end
+
+    def draw
+    end
+  end
+
   class SpielElement
     attr_reader :x_pos, :y_pos
     def initialize(x=1, y=1)
       self.x_pos=x
       self.y_pos=y
       init_sprite
-    end
-
-    def update
     end
 
     def draw
@@ -43,6 +48,29 @@ module Berufsorientierungstag
   end
 
   class Spieler < SpielElement
+    def initialize(x=1, y=1)
+      self.x_pos=x
+      self.y_pos=y
+      @angle=0
+      init_sprite
+    end
+
+    def vor!
+      case @angle
+      when 0 then @x_pos[0] += 1
+      when 90 then @y_pos[0] += 1
+      when 180 then @x_pos[0] -= 1
+      when 270 then @y_pos[0] -= 1
+      end
+    end
+
+    def dreh_links!
+      @angle -= 90
+    end
+
+    def dreh_rechts!
+      @angle += 90
+    end
   end
 
   class Spielfeld
@@ -67,6 +95,30 @@ module Berufsorientierungstag
     def init_feld
       (0...20).each do |num_1|
         (0...20).each do |num_2|
+          self.feld[num_1][num_2] = DummyElement.new
+        end
+      end
+
+      (0..0).each do |num_1|
+        (0...20).each do |num_2|
+          self.feld[num_1][num_2] = SpielElement.new(num_1, num_2)
+        end
+      end
+
+      (19..19).each do |num_1|
+        (0...20).each do |num_2|
+          self.feld[num_1][num_2] = SpielElement.new(num_1, num_2)
+        end
+      end
+
+      (0...20).each do |num_1|
+        (0..0).each do |num_2|
+          self.feld[num_1][num_2] = SpielElement.new(num_1, num_2)
+        end
+      end
+
+      (0...20).each do |num_1|
+        (19..19).each do |num_2|
           self.feld[num_1][num_2] = SpielElement.new(num_1, num_2)
         end
       end
