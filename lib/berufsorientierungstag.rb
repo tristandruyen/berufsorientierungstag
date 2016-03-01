18abe821401e86495bd459da75f437ed033f246f
Tristan Druyen <tristan.druyen@invision.de>
2016-03-01 13:24:44 +0100

    Add Barebone Gosu Stuff

--- lib/berufsorientierungstag.rb
+++ lib/berufsorientierungstag.rb
@@ -1,4 +1,25 @@
 require "berufsorientierungstag/version"
 
 module Berufsorientierungstag
+  require 'gosu'
+
+  class MainWindow < Gosu::Window
+    def initialize
+      super(800, 600)
+      self.caption = 'Hello World!'
+    end
+
+    def update
+    end
+
+    def draw
+    end
+  end
+
+  class << self
+    def start
+      window = MainWindow.new
+      window.show
+    end
+  end
 end
