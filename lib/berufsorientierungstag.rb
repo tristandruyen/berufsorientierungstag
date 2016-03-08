2b618487629e924b4f70b79669e66017678883c5
Tristan Druyen <tristan.druyen@invision.de>
2016-03-08 14:05:15 +0100

    Add Console Shortcut and "Slowness"

--- lib/berufsorientierungstag.rb
+++ lib/berufsorientierungstag.rb
@@ -18,8 +18,10 @@ module Berufsorientierungstag
       super(800, 800)
       self.caption = 'Invision BOT'
       @spielfeld = Spielfeld.new
-      @spielfeld.import_map('maps/default_map.json')
+      # @spielfeld.import_map('maps/default_map.json')
+      @spielfeld.import_map('maps/bot_hard1_map.json')
       @spielfeld.add_player(1, 1)
+      @speed=5
     end
 
     def needs_cursor?
@@ -41,16 +43,27 @@ module Berufsorientierungstag
         @spielfeld.export_map('maps/') if @edit_mode
       elsif Gosu.button_down?(Gosu::KbE)
         @edit_mode = !@edit_mode
+      elsif Gosu.button_down?(Gosu::KbC)
+        require 'pry'
+        binding.pry
       end
     end
     # rubocop:enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
 
     def handle_game_state
-      @spielfeld.call_all unless @edit_mode
+      every_n_times(5) do
+        @spielfeld.call_all unless @edit_mode
+      end
     end
 
     def draw
       @spielfeld.draw_all
+    end
+
+    def every_n_times(n)
+      @every_n_times_count ||= 0
+      yield if (@every_n_times_count % n)==0
+      @every_n_times_count += 1
     end
   end
 
