fa79aad5d4511e21d0214050792f7b363286e301
Tristan Druyen <tristan.druyen@invision.de>
2016-03-11 17:10:39 +0100

    Keybindings

--- lib/input.rb
+++ lib/input.rb
@@ -9,6 +9,13 @@ module Input
       @spielfeld = Spielfeld.new
       @spielfeld.import_map(@maps.first)
       @maps.rotate!
+    when Gosu::KbR
+      @spielfeld = Spielfeld.new
+      @spielfeld.import_map(@maps.last)
+    when Gosu::KbP
+      @maps.rotate! -1
+      @spielfeld = Spielfeld.new
+      @spielfeld.import_map(@maps.last)
       # when Gosu::MsLeft
       #   @spielfeld.edit_mouse_click(mouse_x, mouse_y)
       # when Gosu::MsRight
